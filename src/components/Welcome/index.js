86303b7169eda1c0c7a97ed168ebf201f9d66e1c
Sasikumar <sasieng5851@gamil.com>
2023-09-26 13:52:47 +0000

    change subcribe button text while click the button

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,34 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  buttonText = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const buttonText = this.getButtonText()
+
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <h1 className="title">Welcome</h1>
+          <p className="description">Thank you! Happy Learning</p>
+          <button className="button" type="button" onClick={this.buttonText}>
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Welcome
